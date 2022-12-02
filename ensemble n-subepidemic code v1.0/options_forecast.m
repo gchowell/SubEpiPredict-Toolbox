3f117b18d724cb3550acf07dce23889729d4c5d3
gchowell <31742415+gchowell@users.noreply.github.com>
2022-12-02 15:38:56 -0500

    Add files via upload

--- ensemble n-subepidemic code v1.0/options_forecast.m
+++ ensemble n-subepidemic code v1.0/options_forecast.m
@@ -6,17 +6,18 @@ function [getperformance, deletetempfiles, forecastingperiod, printscreen1, weig
 % <========================== Forecasting parameters ===================================>
 % <==============================================================================>
 
-getperformance=1; % flag or indicator variable (1/0) to calculate forecasting performance or not
+getperformance=1; % flag or indicator variable (1/0) to calculate forecasting performance metrics or not
 
-deletetempfiles=1; %flag or indicator variable (1/0) to delete Forecast..mat files after use
+deletetempfiles=1; %flag or indicator variable (1/0) to indicate whether we wan to delete Forecast..mat files after use
 
-forecastingperiod=30; %forecast horizon (number of data points ahead)
+forecastingperiod=30; % forecast horizon (number of time units ahead)
 
-printscreen1=1;  % print plots with the results
+printscreen1=1;  % flag or indicator variable (1/0) to indicate whether we weant to print plots with the results
 
 % <==============================================================================>
 % <====================== weighting scheme for ensemble model ============================>
 % <==============================================================================>
 
-weight_type1=1; % -1= equally weighted from the top models, 0=based on AICc, 1= based on relative likelihood (Akaike weights), 2=based on WISC during calibration, 3=based on WISF during forecasting performance at previous time period (week)
+weight_type1=1; % -1= equally weighted from the top models, 0= weighted ensemble based on AICc, 1= weighted ensemble based on relative likelihood (Akaike weights), 
+% 2=weighted ensemble based on WISC during calibration, 3=weighted ensemble based on WISF during forecasting performance at previous time period (week)
 
